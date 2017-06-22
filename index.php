ebf61e4987c8e79fcfbe3294145660b8377dfa97
wesandradealves <wesandradealves@gmail.com>
2017-06-22 05:15:21 -0300

    Arquivos do base-template

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.css" type="text/css" media="all" />
+        <link rel="stylesheet" href="./node_modules/materialize-css/dist/css/materialize.css" type="text/css" media="all" />
+        <link rel="stylesheet" href="./node_modules/css-reset/reset.css" type="text/css" media="all" />
+        <link rel="stylesheet" href="./style.css" type="text/css" media="all" />
+    </head>
+    <body>
+        <div id="wrap">
+            <header>
+                
+            </header>
+            <main>
+                
+            </main>
+        </div>
+        <footer>
+            <div class="container">
+        
+            </div>
+        </footer>
+        <script src="./node_modules/jquery/dist/jquery.min.js" type="text/javascript"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <script src="./node_modules/materialize-css/dist/js/materialize.min.js" type="text/javascript"></script>
+        <script src="./assets/js/transformicons.min.js" type="text/javascript"></script>
+        <script src="./assets/js/inview.js" type="text/javascript"></script>
+        <script src="./assets/js/functions.js" type="text/javascript"></script>
+        <script>
+        transformicons.add('.tcon')
+        </script>
+    </body>
+</html>
